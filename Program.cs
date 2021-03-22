67e6bc794cab286657a0a3c65d0d30af97000b54
Thato Ramalepa <tramalepa@my.wctc.edu>
2021-03-22 13:16:01 -0500

    csv

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+//using System.Web.Mvc. Name.Controllers
+//{
 
+   // using Microsoft.AspNetCore.Mvc;
+
+   // [Route("api/[controller]")]
+   // [ApiController]
+   // public class NameController : ControllerBase
+    {
+      //  [HttpGet]
+        //public async Task<IActionResult> Get()
+        {
+            
+            //return Ok();
+        }
+    }
+}
 namespace Movies
 {
     class Program
@@ -9,5 +25,19 @@ namespace Movies
         {
             Menu.Choice();
         }
+
+       // Public Class HomeController
+
+//        Inherits System.Web.Mvc.Controller 
+
+       // Private _db As New MoviesDBEntities() 
+
+        //Function Index() As ActionResult
+
+          //  Return View(_db.MovieSet.ToList())
+
+       // End Function
+
+   // End Class
     }
 }
